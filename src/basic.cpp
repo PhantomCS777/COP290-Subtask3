cb13396204669448f05e90316bad275eec95f325
Sah-il-Kumar <sahil984sk@gmail.com>
2024-02-08 00:06:44 +0530

    basic

---
+++ src/basic.cpp
@@ -0,0 +1,96 @@
+#include "basic.h"
+std::string reverse(std::string str){
+    std::string ans=str.substr(str.length() - 4); 
+    ans+=str.substr(3,2);
+    ans+=str.substr(0,2);
+    return ans;
+}
+
+std::string replace_hyphens(std::string str){
+    std::string ans="";
+    for(auto c:str){
+        if(c=='-') ans+='/';
+        else ans+=c;
+    }
+    return ans;
+}
+Output basic(std::vector <StockData> stockdata,int n,std::string start_date,std::string end_date,int limit)
+{
+    std::vector <Cash_flow> daily;
+    std::vector <Order_stats> order;
+    start_date=reverse(start_date);
+    end_date=reverse(end_date);
+    int curposition=0;
+    int cur_trend=0;
+    int trend_days=0;
+    int total_data_size = stockdata.size();
+    long long final_profit_loss=0;
+    int cur_price;
+    for(int i=total_data_size-2;i>=0;i--){
+        std::string modified_date = reverse(stockdata[i].date);
+        if(stockdata[i].close==stockdata[i+1].close){
+            cur_trend=0;
+            trend_days=0;
+        }
+        else if(stockdata[i].close>stockdata[i+1].close){
+            if(cur_trend==0){
+                cur_trend=1;
+                trend_days=1;
+            }
+            else if(cur_trend==1){
+                trend_days++;
+            }
+            else{
+                cur_trend=1;
+                trend_days=1;
+            }
+        }
+        else{
+            if(cur_trend==-1){
+                trend_days++;
+            }
+            else{
+                cur_trend=-1;
+                trend_days=1;
+            }
+        }
+        if(modified_date>=start_date&&modified_date<=end_date){
+            cur_price=stockdata[i].close;
+            if(trend_days>=n){
+                if(cur_trend==-1&&(curposition>(-1*limit))){
+                    curposition--;
+                    Cash_flow a(modified_date,stockdata[i].close);
+                    daily.emplace_back(a);
+                    Order_stats b(modified_date,"SELL",1,stockdata[i].close);
+                    order.emplace_back(b);
+                    final_profit_loss+=stockdata[i].close;
+                }
+                else if(cur_trend==1 && (curposition<limit)){
+                    curposition++;
+                    Cash_flow a(modified_date,-1*stockdata[i].close);
+                    daily.emplace_back(a);
+                    Order_stats b(modified_date,"BUY",1,stockdata[i].close);
+                    order.emplace_back(b);
+                    final_profit_loss-=stockdata[i].close;
+                }
+                else{
+                    Cash_flow a(modified_date,0);
+                    daily.emplace_back(a);
+                }
+            }
+            else{
+                Cash_flow a(modified_date,0);
+                daily.emplace_back(a);
+            }
+        }
+    }
+    if(curposition>0){
+        final_profit_loss+=(curposition*cur_price);
+    }
+    else{
+        final_profit_loss-=(curposition*cur_price);
+    }
+    Output out(final_profit_loss,daily,order);
+    return out;
+}
+
--- src/stock_data.h
+++ src/stock_data.h
@@ -1,7 +1,4 @@
 #include <bits/stdc++.h> 
-
-
-
 struct StockData 
 {
     std::string date; 
@@ -23,3 +20,63 @@ struct StockData
     double no_of_trades; 
     
 };
+
+struct Input
+{
+    std::string strategy; 
+    std::string symbol; 
+    std::string start_date; 
+    std::string end_date; 
+    std::string train_start_date; 
+    std::string train_end_date;
+     int n; 
+     int x; 
+     double p; 
+     int max_hold_days; 
+     double c1; 
+     double c2; 
+     double oversold_threshold; 
+     double overbought_threshold;
+     double adx_threshold;
+     double threshold; 
+     
+
+};
+
+class Cash_flow{
+    public:
+    std::string date;
+    double transaction;
+
+    Cash_flow(std::string date,double transaction){
+        date=date;
+        transaction=transaction;
+    }
+};
+
+class Order_stats{
+    public:
+    std::string date;
+    std::string order_dir;
+    int quantity;
+    double price;
+    Order_stats(std::string date,std::string order_dir,int quantity,double price){
+        date=date;
+        order_dir=order_dir;
+        quantity=quantity;
+        price=price;
+    }
+};
+
+class Output{
+    public:
+    double final_profit_loss;
+    std::vector <Cash_flow> daily;
+    std::vector <Order_stats> order;
+    Output(double final_profit_loss,std::vector <Cash_flow> daily,std::vector<Order_stats> order){
+        final_profit_loss=final_profit_loss;
+        daily=daily;
+        order=order;
+    }
+};
+
