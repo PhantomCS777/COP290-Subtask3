7a7f202e7a8cdad94354109679f302e73d1cd5f4
Hemanshu Garg <garghemanshu777@gmail.com>
2024-02-11 15:20:07 +0530

    best of all update

--- src/simple_pairs.h
+++ src/simple_pairs.h
@@ -2,4 +2,4 @@
 #include "stock_data.h"
 #include <bits/stdc++.h>
 
-Output simple_pairs (std::vector <StockData>,Input) ;
+Output simple_pairs (std::vector <StockData> stockData1,std::vector <StockData> stockData2,Input) ;
